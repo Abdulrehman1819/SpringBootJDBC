9af6f3b40d55006bee931f0f4432401d100fa537
Abdul Rehman <abdulrehman18197@gmail.com>
2026-04-23 22:21:49 +0500

    Spring JDBC

--- src/main/java/com/abd/JDBCDEMO/repo/StudentRepo.java
+++ src/main/java/com/abd/JDBCDEMO/repo/StudentRepo.java
@@ -21,7 +21,7 @@ public class StudentRepo {
     private JdbcTemplate jdbcTemplate;
     private Student s;
     public void save(Student s){
-        String sql="insert  into students (roll_no,name,marks) values(?,?,?)";
+        String sql="insert  into student (rollno,name,marks) values(?,?,?)";
         int rows=jdbcTemplate.update(sql,s.getRollNo(),s.getName(),s.getMarks());
         System.out.println(rows+"Effected");
         System.out.println("Added");
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into  student (rollno,name,marks) values (101,'Abdulrehman',20);
+insert into  student (rollno,name,marks) values (112,'Abdulrehman',20);
+insert into  student (rollno,name,marks) values (113,'Abdulrehman',20);
